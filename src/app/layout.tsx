f83f6312fc09ad8de31aba936863fba5f80d7b8a
igor.kudlo <igoreklida@gmail.com>
2026-01-25 11:58:36 +0300

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Rubik } from "next/font/google";
+import "./globals.css";
+
+const rubik = Rubik({
+  variable: "--font-rubik",
+  subsets: ["latin", "cyrillic"],
+});
+
+
+export const metadata: Metadata = {
+  title: "Северяночка",
+  description: "Доставка и покупка продуктов питания",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${rubik.variable} font-sans`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
