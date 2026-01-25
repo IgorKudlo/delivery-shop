47565cecd6fc93a9707e6c097dc0cc79af5ddb03
igor.kudlo <igoreklida@gmail.com>
2026-01-25 20:55:03 +0300

    lesson_2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Rubik } from "next/font/google";
 import "./globals.css";
+import Footer from "@/components/Footer";
+import Header from "@/components/header/Header";
 
 const rubik = Rubik({
   variable: "--font-rubik",
@@ -23,7 +25,9 @@ export default function RootLayout({
       <body
         className={`${rubik.variable} font-sans`}
       >
+        <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
